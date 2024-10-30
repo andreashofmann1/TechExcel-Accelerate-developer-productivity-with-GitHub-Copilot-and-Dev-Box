ae0981e829e4735e805ae4393f16666deebf32d0
andreashofmann1 <andreas.hofmann@ku7t.org>
2024-10-30 03:51:06 -0700

    Resolves #7

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,9 +7,22 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message text.
+        /// </summary>
+        /// <value>
+        /// The message text, limited to 250 characters.
+        /// </value>
+        /// <remarks>
+        /// This property is required and must be a text string. 
+        /// If the message exceeds 250 characters, an error will be thrown.
+        /// </remarks>
+        /// <exception cref="ValidationException">
+        /// Thrown when the message exceeds 250 characters.
+        /// </exception>
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion
